1569928a4909d33df04a7cbd19387978f6ddb19a
owlyyy <arquxx@gmail.com>
2026-06-19 22:17:07 +0200

    a123

--- AudioRouting/MojaveAudioSentinel.cs
+++ AudioRouting/MojaveAudioSentinel.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
-namespace F76World.Launcher.AudioRouting
+namespace f76World.AudioRouting
 {
     /// <summary>
     /// Represents an available physical audio endpoint in the system.
